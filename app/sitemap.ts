365039eec2ce44c18759c830d5a6593e1d5482ac
liz <lizalfond@gmail.com>
2026-07-07 13:56:59 +0700

    Add story page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -34,6 +34,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.7,
     },
     {
+      url: `${BASE_URL}/story`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
       url: `${BASE_URL}/welcome`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
